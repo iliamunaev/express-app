ded6f34121c92f489f38a13e8fa844f63bf615ab
Ilia Munaev <ilyamunaev@gmail.com>
2024-05-27 18:57:36 +0300

    testing watchtower #2

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Watchtower passed test successfully!!</h1>')
+  res.send('<h1>We are the champions!</h1>' +
+      '<link>https://www.youtube.com/watch?v=KXw8CRapg7k&ab_channel=QueenVEVO')
 })
 
 const PORT = 8080
