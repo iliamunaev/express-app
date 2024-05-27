0cc72f7172dc0ff9d5bb8421b341a56288dc98e6
Ilia Munaev <ilyamunaev@gmail.com>
2024-05-27 19:26:49 +0300

    test #5

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Tests passed successfully!</h1>' +
+  res.send('<h1>We are the champions!</h1>' +
       '<a href="https://www.youtube.com/watch?v=KXw8CRapg7k&ab_channel=QueenVEVO">Queen - We Are The Champions</a>')
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running at http://localhost:${PORT}`)
 })
